557389224cdd2b3656905b28bfa2fdbfdabc4036
Kevin Cuesta <CRACKESAN@GMAIL.COM>
2024-05-20 19:56:14 -0500

    Parcial back-front

---
+++ shopping-cart/src/router/index.ts
@@ -0,0 +1,40 @@
+import { createRouter, createWebHistory } from '@ionic/vue-router';
+import { RouteRecordRaw } from 'vue-router';
+import HomePage from '../views/HomePage.vue'
+import DasboardPage from '../views/DasboardPage.vue'
+import ClientePage from '@/views/ClientePage.vue';
+import CategoriaPage from '@/views/CategoriaPage.vue';
+
+const routes: Array<RouteRecordRaw> = [
+  {
+    path: '/',
+    redirect: '/home'
+  },
+  {
+    path: '/home',
+    name: 'Home',
+    component: HomePage
+  },{
+    path: '/cliente',
+    name: 'Cliente',
+    component: ClientePage
+  },
+  {
+    path: '/dashboard',
+    name: 'Dashboard',
+    component: DasboardPage
+  },
+  {
+    path: '/categoria',
+    name: 'Categoria',
+    component: CategoriaPage
+  }
+
+]
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes
+})
+
+export default router
---
+++ shopping-cart/src/views/DasboardPage.vue
@@ -0,0 +1,26 @@
+<template>
+  <ion-page>
+    <ion-header :translucent="true">
+      <ion-toolbar>
+        <ion-title>Dashboard</ion-title>
+      </ion-toolbar>
+    </ion-header>
+
+    <ion-content>
+      <br>
+      <div id="container">
+        <strong><a href="/categoria">Ir a Categoria</a></strong>
+        <br>
+        <strong><a href="/carro">Ir a Carro</a></strong>
+
+      </div>
+
+    </ion-content>
+  </ion-page>
+</template>
+
+<script setup lang="ts">
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/vue';
+</script>
+
+<style scoped></style>
